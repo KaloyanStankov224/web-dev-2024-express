c4c9823f77665a2a58bc9c5ab1d1a2a263721da8
KaloyanStankov224 <kaloqn.t.st@gmail.com>
2024-11-20 22:45:23 +0200

    ad

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,5 +1,6 @@
-import { DataTypes, Model, Sequelize, Optional, HasOneSetAssociationMixin, HasOneGetAssociationMixin } from 'sequelize';
+import { DataTypes, Model, Sequelize, Optional, HasOneSetAssociationMixin, HasOneGetAssociationMixin, BelongsToManyGetAssociationsMixin, BelongsToManySetAssociationsMixin } from 'sequelize';
 import { University } from '../university/university';
+import { Subject } from '../subjects/subjects';
 
 interface UserAttributes {
   id: number;
@@ -17,6 +18,8 @@ export class User extends Model<UserAttributes, UserCreationAttributes> implemen
   public email!: string;
   declare setUniversity: HasOneSetAssociationMixin<University, number>;
   declare getUniversity: HasOneGetAssociationMixin<University>;
+  declare getSubjects: BelongsToManyGetAssociationsMixin<Subject>;
+  declare setSubjects: BelongsToManySetAssociationsMixin<Subject, number>;
 }
 
 export const UserModel = (sequelize: Sequelize) => {
--- test-post.js
+++ test-post.js
@@ -18,14 +18,40 @@ async function testPostUserRequest() {
     const response = await axios.post('http://localhost:3000/user', {
       name: 'John Doe',
       email: 'johndoe@example.com',
-      universityId: 1
+      universityId: 1,
+      subjects: [1, 2]
     });
     console.log('Response data:', response.data);
   } catch (error) {
     console.error('Error:', error.response ? error.response.data : error.message);
   }
 }
+
+async function testCreateSubject(name) {
+  try {
+    const response = await axios.post('http://localhost:3000/subjects/create', {name: name})
+    console.log('All subjects: ', response.data);
+  }catch(error){
+    console.error('Error:', error.response ? error.response.data : error.message);
+  }
+}
+
+async function testUpdateDBSubjects() {
+  try{
+    const response = await axios.put('http://localhost:3000/user/subjects', {
+      userId: 1,
+      subjects: [1, 2, 3]
+    })
+  }catch(error){
+    console.error('Error:', error.response ? error.response.data : error.message);
+  }
+}
+
 (async () => {
   await testPostUniversityRequest();
-  await testPostUserRequest()
-})()
+  await testCreateSubject("Math");
+  await testCreateSubject("Chem");
+  await testCreateSubject("Bio");
+  await testPostUserRequest();
+  await testUpdateDBSubjects();
+})();
